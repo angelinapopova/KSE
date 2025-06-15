86b01b724363f567d5b091e70b631803da2a7280
angelinapopova <apopova@kse.org.ua>
2025-06-15 22:48:50 +0300

    wordle.py

--- wordle.py
+++ wordle.py
@@ -1,17 +1,22 @@
 import word_generator
 import word_input
 import results
-generated_word = word_generator.generate_word()
-max_attempts = 7
+while True:
+    generated_word = word_generator.generate_word()
+    max_attempts = 7
 
-for attempt in range(max_attempts):
-    print(f"Attempt {attempt + 1}/{max_attempts}")
+    for attempt in range(max_attempts):
+        print(f"Attempt {attempt + 1}/{max_attempts}")
     
-    user_word = word_input.word_input()
-    result = results.check_word(user_word, generated_word)
-    print(result)
-    if user_word == generated_word:
-        print("Congradulations!")
+        user_word = word_input.word_input()
+        result = results.check_word(user_word, generated_word)
+        print(result)
+        if user_word == generated_word:
+            print("Congradulations!")
+            break
+    else:
+        print("Soory, you're a loser:(")
+    play_again = input("Do you want to play again? ")
+    if play_again != "yes":
         break
-else:
-    print("Soory, you're a loser:(")
+    
